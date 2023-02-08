16e398c605e9a7c5edf08326d36d722dc7c86e52
formidablae <81068781+formidablae@users.noreply.github.com>
2023-02-08 22:36:56 +0100

    Did 1 Hello World / 1.2 Planting a Tree Project

---
+++ 1_Hello_World/1.2_Planting_a_Tree_Project/Tree.java
@@ -0,0 +1,13 @@
+public class Tree {
+	public static void main(String[] args) {
+		// print a message introducing yourself to the user
+		System.out.println("Hey there, I'm Ariel and I'm learning to code in Java!");
+		System.out.println("I'm going to plant a tree today!");
+		System.out.println("");
+		System.out.println("    *    ");
+    System.out.println("   ***   ");
+    System.out.println("  *****  ");
+    System.out.println(" ******* ");
+    System.out.println("    *    ");
+	}
+}
