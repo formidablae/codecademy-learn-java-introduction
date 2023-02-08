9ef4f0a82de38a6616e39f45191d3dcc299f6116
formidablae <81068781+formidablae@users.noreply.github.com>
2023-02-08 22:05:21 +0100

    Did 1 Hello World / 1.1 Hello World

---
+++ 1_Hello_World/1.1_Hello_World/1.1.6_Compilation_Catching_Errors/commands.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd 1_Hello_World/1.1_Hello_World/1.1.6_Compilation_Catching_Errors
+javac Compiling.java
+ls
+javac Compiling.java
+cd ../..
---
+++ 1_Hello_World/1.1_Hello_World/1.1.7_Compilation_Creating_Executables/commands.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd 1_Hello_World/1.1_Hello_World/1.1.7_Compilation_Creating_Executables
+ls
+javac Compiling.java
+ls
+java Compiling
+cd ../..
