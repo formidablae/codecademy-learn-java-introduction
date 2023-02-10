5f3846955b9f3af53058ed0b7e008822763ad540
formidablae <81068781+formidablae@users.noreply.github.com>
2023-02-10 11:07:33 +0100

    Did 2 Variables / 24 Math Magic Project

---
+++ 2_Variables/2.4_Math_Magic_Project/Magic.java
@@ -0,0 +1,13 @@
+public class Magic {
+	public static void main(String[] args) {
+    int myNumber = 3;
+    int stepOne;
+    stepOne = myNumber * myNumber;
+    int stepTwo = stepOne + myNumber;
+    int stepThree = stepTwo / myNumber;
+    int stepFour = stepThree + 17;
+    int stepFive = stepFour - myNumber;
+    int stepSix = stepFive / 6;
+    System.out.println(stepSix);
+	}
+}
